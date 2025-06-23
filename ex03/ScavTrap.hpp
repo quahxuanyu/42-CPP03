df6d0f3858a8ef5e759c1a1f526e964939d6a233
Xuanyu Quah <xquah@u82z05s02.42kl.edu.my>
2025-06-23 13:21:03 +0800

    minor changes to ex03/ScavTrap.hpp

--- ex03/ScavTrap.hpp
+++ ex03/ScavTrap.hpp
@@ -7,14 +7,14 @@ class ScavTrap: virtual public ClapTrap {
 		static const unsigned int defaultHitPoints = 100;
 		static const unsigned int defaultEnergyPoints = 50;
 		static const unsigned int defaultAttackDamage = 20;
-    public:
-        ScavTrap(); // 1. Default Constructor
-        ScavTrap(std::string name); // EXTRA. Parametized Constuctor
-        ScavTrap(const ScavTrap &other); // 2. Copy Constructor
-        ScavTrap &operator=(const ScavTrap &other); // 3. Copy Assignment Operator
-        ~ScavTrap(); // 4. Destructor
-        void attack(const std::string &target);
-        void guardGate();
+  public:
+    ScavTrap(); // 1. Default Constructor
+    ScavTrap(std::string name); // EXTRA. Parametized Constuctor
+    ScavTrap(const ScavTrap &other); // 2. Copy Constructor
+    ScavTrap &operator=(const ScavTrap &other); // 3. Copy Assignment Operator
+    ~ScavTrap(); // 4. Destructor
+    void attack(const std::string &target);
+    void guardGate();
 };
 
 #endif
